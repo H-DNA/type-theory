2f4a419e5fd4ad7c0f67bbe44e048fe83f9d37d6
Huy-DNA <huydo862003@gmail.com>
2026-01-03 21:41:49 +0700

    feat: add tuple unwrapping in rocq

--- sf/lf/ch01-basics/tuples.v
+++ sf/lf/ch01-basics/tuples.v
@@ -1,5 +1,9 @@
 (* vim:ft=coq *)
 
+Inductive bool : Type :=
+  | true
+  | false.
+
 Module TuplePlayground.
   (* bit datatype that represents bool *)
   Inductive bit : Type :=
@@ -11,4 +15,11 @@ Module TuplePlayground.
     | bits (b0 b1 b2 b3 : bit). (* Tuple definition *)
 
   Check (bits B1 B0 B1 B0) : nybble.
+
+  (* Unwrap tuple *)
+  Definition all_zero (nb : nybble) : bool :=
+    match nb with
+    | (bits B0 B0 B0 B0) => true
+    | (bits _ _ _ _) => false
+    end.
 End TuplePlayground.
