e11447c12ed0d621c2108265a33fa08f7a1e7093
Huy-DNA <huydo862003@gmail.com>
2026-01-05 21:51:50 +0700

    feat: add ltb to rocq

---
+++ sf/lf/ch01-basics/exercises/ltb.v
@@ -0,0 +1,26 @@
+(* vim:ft=coq *)
+
+Inductive nat : Type :=
+  | O
+  | S (n : nat).
+
+Fixpoint ltb (n m : nat) : bool :=
+  match n with
+  | O => match m with
+         | O => false
+         | _ => true
+         end
+  | S n' => match m with
+            | O => false
+            | S m' => ltb n' m'
+            end
+  end.
+
+Notation "x <? y" := (ltb x y)  (at level 70) : nat_scope.
+
+Example test_ltb1 : (ltb 2 2) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_ltb2 : (ltb 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_ltb3 : (ltb 4 2) = false.
+Proof. simpl. reflexivity. Qed.
